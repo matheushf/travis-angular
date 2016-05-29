e39df54afeb535ff224f560de51faa4f25abcc69
Matheus Victor <hffmatheus@gmail.com>
2016-05-29 20:17:02 -0300

    complement

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,11 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         jshint: {
-            all: ['Gruntfile', 'hello.js']
+            all: ['Gruntfile.js', 'hello.js']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.registerTask('default', 'jshint');
-    
+
 };
