3f817f6a6141aa58f0a8be6b50f1446b9913c9f4
Matheus Victor <hffmatheus@gmail.com>
2016-05-30 23:07:21 -0300

    karma

---
+++ test/unit/versionSpec.js
@@ -0,0 +1,8 @@
+#!javascript
+describe('version', function() {
+    beforeEach(module('myApp.services'));
+
+    it('should return current version', inject(function(version) {
+        expect(version).toEqual('0.1');
+    }));
+});
